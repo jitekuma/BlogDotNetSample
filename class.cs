d8edd3d95df620318af1f321c07869f271831f2c
Jitendra Kumar <jikuma@microsoft.com>
2016-12-29 21:32:02 +0530

    update the test

--- class.cs
+++ class.cs
@@ -8,17 +8,29 @@ namespace MyFirstDotNetCoreTests
         public void PassingTest()
         {
             Assert.Equal(4, Add(2, 2));
-        }
-
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
+			Assert.Equal(0, Subtract(2, 2));
+			Assert.Equal(4, Multiply(2, 2));
+			Assert.Equal(1, Divide(2, 2));
         }
 
         int Add(int x, int y)
         {
             return x + y;
+        }
+		
+		int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+		
+		int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+		
+		int Divide(int x, int y)
+        {
+            return x / y;
         }
     }
 }
